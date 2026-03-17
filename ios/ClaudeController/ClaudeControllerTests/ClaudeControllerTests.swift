ced6973b2d0a6cebd1c57cbd651b669bd0e9598a
Jay Chinthrajah <lankeami@gmail.com>
2026-03-17 13:57:01 -0400

    fix: add Xcode project and consolidate iOS source files

---
+++ ios/ClaudeController/ClaudeControllerTests/ClaudeControllerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClaudeControllerTests.swift
+//  ClaudeControllerTests
+//
+//  Created by Jay Chinthrajah on 3/17/26.
+//
+
+import Testing
+@testable import ClaudeController
+
+struct ClaudeControllerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
